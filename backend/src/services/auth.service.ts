acdcfde04647754fd7fc0ac7c49c47f36725afef
aditehh <aditya.aryal888@gmail.com>
2026-05-09 00:49:52 +0545

    duplicate emails error

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -2,10 +2,21 @@ import { prisma } from "../db";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+
 const JWT_SECRET = process.env.JWT_SECRET!;
 console.log("authservice jwt is ",JWT_SECRET)
 
 export const register = async (email: string, password: string) => {
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+  if(existingUser) {
+    throw new Error(
+      "user already exists"
+    )
+    
+  }
 
   const hashed = await bcrypt.hash(password, 10);
 
