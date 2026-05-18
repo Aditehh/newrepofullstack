1e7c86d49e5183aff346aba063460542a0094a92
aditehh <aditya.aryal888@gmail.com>
2026-05-18 23:39:19 +0545

    pagination

--- backend/src/controllers/note.controller.ts
+++ backend/src/controllers/note.controller.ts
@@ -5,8 +5,21 @@ import { createNoteSchema, updateNoteSchema } from "../validators/note.validator
 
 
 export const getAllNotes = async (req: Request, res: Response) => {
+
     const userId = (req as any).userId;
-    const notes = await noteService.getNotes(userId);
+
+    const page = Number(req.query.page) || 1;
+
+    const limit = Number(req.query.limit) || 10;
+
+    const search = String(req.query.search) || ";"
+
+    const notes = await noteService.getNotes(
+        userId,
+        page,
+        limit,
+        search
+    );
     res.json(notes);
 }
 
